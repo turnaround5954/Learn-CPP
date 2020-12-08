86217dda693d8017ccfe3b6067abcfcdcc8c25ce
turnaround5954 <404305602@qq.com>
2020-12-09 03:00:17 +0800

    add binomial-heap

--- cs106x/assign-5-pqueue/src/pqueue-binomial-heap.cpp
+++ cs106x/assign-5-pqueue/src/pqueue-binomial-heap.cpp
@@ -1,28 +1,124 @@
 #include "pqueue-binomial-heap.h"
+#include "error.h"
 using namespace std;
 
 BinomialHeapPQueue::BinomialHeapPQueue() {}
-BinomialHeapPQueue::~BinomialHeapPQueue() {}
+BinomialHeapPQueue::~BinomialHeapPQueue() {
+    for (Node *tree : TreeVec) {
+        if (tree != NULL) {
+            freeTree(tree);
+        }
+    }
+}
 
-const string& BinomialHeapPQueue::peek() const {
+void BinomialHeapPQueue::freeTree(Node *root) {
+    while (!root->children.isEmpty()) {
+        Node *child = root->children.removeBack();
+        freeTree(child);
+    }
+    delete root;
+}
+
+int BinomialHeapPQueue::findMinIdx(const Vector<Node *> &vec) {
+    int minIdx = 0;
+    int startIdx = 0;
+    while (startIdx < vec.size() && vec[startIdx] == NULL) {
+        startIdx++;
+    }
+    minIdx = startIdx;
+    for (int i = startIdx; i < vec.size(); i++) {
+        if (vec[i] != NULL && vec[i]->data < vec[minIdx]->data) {
+            minIdx = i;
+        }
+    }
+    return minIdx;
+}
+
+const string &BinomialHeapPQueue::peek() const {
     // placeholder so method compiles..
-	// replace with your own implementation
-    return kEmptyString;
+    // replace with your own implementation
+    if (isEmpty()) {
+        error("peek: Attempting to peek at an empty queue");
+    }
+    int minIdx = findMinIdx(TreeVec);
+    return TreeVec[minIdx]->data;
 }
 
 string BinomialHeapPQueue::extractMin() {
-	// placeholder so method compiles..
-	// replace with your own implementation
-	return peek();
-}
-
-void BinomialHeapPQueue::enqueue(const string& /* elem */) {
     // placeholder so method compiles..
-	// replace with your own implementation
+    // replace with your own implementation
+    if (isEmpty()) {
+        error("peek: Attempting to peek at an empty queue");
+    }
+    int minIdx = findMinIdx(TreeVec);
+    string minVal = TreeVec[minIdx]->data;
+    Vector<Node *> aux = TreeVec[minIdx]->children;
+    delete TreeVec[minIdx];
+    TreeVec[minIdx] = NULL;
+    mergeTreeVec(TreeVec, aux);
+    logSize--;
+    return minVal;
 }
 
-BinomialHeapPQueue *BinomialHeapPQueue::merge(BinomialHeapPQueue */* one */, BinomialHeapPQueue * /* two */) {
-	// placeholder so method compiles..
-	// replace with your own implementation
-	return new BinomialHeapPQueue();
+void BinomialHeapPQueue::enqueue(const string &elem) {
+    // placeholder so method compiles..
+    // replace with your own implementation
+    Vector<Node *> aux;
+    aux.add(new Node());
+    aux[0]->data = elem;
+    mergeTreeVec(TreeVec, aux);
+    logSize++;
+}
+
+BinomialHeapPQueue *BinomialHeapPQueue::merge(BinomialHeapPQueue *one,
+                                              BinomialHeapPQueue *two) {
+    // placeholder so method compiles..
+    // replace with your own implementation
+    BinomialHeapPQueue *pq = new BinomialHeapPQueue();
+    pq->logSize = one->logSize + two->logSize;
+    pq->TreeVec = one->TreeVec;
+    mergeTreeVec(pq->TreeVec, two->TreeVec);
+    return pq;
+}
+
+void BinomialHeapPQueue::mergeTreeVec(Vector<Node *> &one,
+                                      Vector<Node *> &two) {
+    int size = max(one.size(), two.size());
+    Node *carry = NULL;
+    while (one.size() < size) {
+        one.add(NULL);
+    }
+    for (int i = 0; i < size; i++) {
+        Node *aux = NULL;
+        if (i < two.size() && two[i] != NULL) {
+            aux = two[i];
+        }
+        if (mergeTree(one[i], aux)) {
+            swap(one[i], carry);
+        } else {
+            if (mergeTree(one[i], carry)) {
+                carry = one[i];
+                one[i] = NULL;
+            } else {
+                carry = NULL;
+            }
+        }
+    }
+
+    if (carry != NULL) {
+        one.add(carry);
+    }
+}
+
+bool BinomialHeapPQueue::mergeTree(Node *&one, Node *two) {
+    if (one == NULL || two == NULL) {
+        one = (one == NULL) ? two : one;
+        return false;
+    }
+
+    if (one->data > two->data) {
+        swap(one, two);
+    }
+    one->children.add(two);
+    return true;
 }
--- cs106x/assign-5-pqueue/src/pqueue-binomial-heap.h
+++ cs106x/assign-5-pqueue/src/pqueue-binomial-heap.h
@@ -1,20 +1,32 @@
 #pragma once
 #include "pqueue.h"
+#include "vector.h"
 #include <string>
 
 class BinomialHeapPQueue : public PQueue {
-public:
-	BinomialHeapPQueue();
-	~BinomialHeapPQueue();
-	
-	static BinomialHeapPQueue *merge(BinomialHeapPQueue *one, BinomialHeapPQueue *two);
-	
-	void enqueue(const std::string& elem);
+  public:
+    BinomialHeapPQueue();
+    ~BinomialHeapPQueue();
+
+    static BinomialHeapPQueue *merge(BinomialHeapPQueue *one,
+                                     BinomialHeapPQueue *two);
+
+    void enqueue(const std::string &elem);
     std::string extractMin();
-    const std::string& peek() const;
-	
-private:
+    const std::string &peek() const;
+
+  private:
     // provide data methods and helper methods to
     // help realize the binomial heap-backed PQueue
-};
+    struct Node {
+        std::string data;
+        Vector<Node *> children;
+    };
 
+    Vector<Node *> TreeVec;
+    static void freeTree(Node *root);
+    // static Node *mergeTree(Node *one, Node *two);
+    static int findMinIdx(const Vector<Node *> &vec);
+    static bool mergeTree(Node *&one, Node *two);
+    static void mergeTreeVec(Vector<Node *> &one, Vector<Node *> &two);
+};
